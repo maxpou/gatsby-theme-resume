911e4660b612d80f6fdba4eed2265e9b04a13af8
Maxence Poutord <maxence.poutord@gmail.com>
2019-08-18 11:28:32 +0200

    docs: anonymize data

--- data/resume.js
+++ data/resume.js
@@ -1,7 +1,7 @@
 module.exports = {
   basics: {
-    name: "Maxence Poutord",
-    about: "(Passionate) Software Engineer • Public speaker • Blogger",
+    name: "John Doe",
+    about: "Software Engineer • Public speaker • Blogger",
     email: "hello@maxpou.fr",
     nationality: "French",
     age: "30",
@@ -27,137 +27,105 @@ module.exports = {
   skills: [
     {
       category: "Proficient",
-      content: ["JavaScript", "Vue.js", "Git", "Testing (Jest)"],
+      content: ["JavaScript", "React.js"],
     },
     {
       category: "Comfortable",
-      content: ["React.js", "Gatsby.js", "Functional Programming", "CI/CD", "PHP"],
+      content: ["Redux", "ReasonML", "Python", "Rust"],
     },
     {
       category: "Familiar",
-      content: ["Node.js", "Docker", "Firebase", "GraphQL", "MongoDB", "SQL"],
+      content: ["Node.js", "Docker", "GraphQL", "MongoDB"],
     },
   ],
 
   projects: [
     {
-      name: "maxpou.fr",
-      description: "my personal website",
-      url: "https://www.maxpou.fr",
+      name: "TC39",
+      description: "",
+      url: "https://github.com/tc39/proposal-nullish-coalescing",
       highlights: [
-        "Write Technical posts (French & English)",
-        "Built with Gatsby, React, Css-in-js and MDX"
+        "Champion for Nullish Coalescing proposal",
       ],
     },
     {
       name: "Public speaker",
       description: "at technical conferences",
-      url: "https://www.maxpou.fr/speaking",
+      url: "",
       highlights: [
-        "Conference topic: JavaScript, Testing, PWA and Software quality",
-        "Workshops/Trainings: Vue.js and PHP (languages)",
+        "Topics addressed: Future of JavaScript, Performances and Testing",
+        "Workshop: React Hooks, testing with Jest",
       ],
     },
     {
-      name: "Open source contributions",
+      name: "Firefox debugger",
       description: "",
-      url: "https://github.com/maxpou",
+      url: "https://github.com/firefox-devtools/debugger",
       highlights: [
-        "gatsby-starter-morning-dew - A Gatsby theme/starter to build lightning-fast blog/websites",
-        "docker-symfony - A multicontainer stack with NGINX, PHP7-FPM, MySQL and ELK (Elasticsearch Logstash and Kibana)",
-        "Other - see my GitHub Profile"
+        "Implemented the algorithm for step-by-step debugging",
+        "Wrote documentation for team and external contributors"
       ],
     },
   ],
 
   experiences: [
     {
-      company: "VSware",
-      position: "Senior Software Engineer",
+      company: "Gatsby.inc",
+      position: "Software Engineer",
       period: "May 2018 - Present",
       location: "Remote",
       activities: [
-        "Worked on large Vue.js application",
-        "Set up a unit and integration testing strategy",
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore a reiciendis nesciunt",
+        "officia voluptas fuga magnam dolorum laudantium, exercitationem, quas asperiores voluptatum",
       ],
     },
     {
-      company: "Hostelworld",
-      position: "Software Engineer",
+      company: "Mozilla",
+      position: "Production Engineer",
       period: "Nov 2016 - Apr 2018",
-      location: "Dublin, Ireland",
+      location: "Berlin, Germany",
       activities: [
-        "Architecture definition, development and maintenance of the main website (back+front)",
-        "Refactor complete frontend architecture of the main website (from jQuery to Vue.js)",
-        "Train and mentor team to modern JavaScript and Vue.js",
-      ],
-    },
-    {
-      company: "Conserto",
-      position: "Consultant",
-      period: "May 2015 - Apr 2016",
-      location: "Nantes, France",
-      activities: [
-        "Architecture definition, development and maintenance of web applications",
-        "Build proof of concepts/research and development",
-        "Draft and give training in PHP, Symfony2",
-      ],
-    },
-    {
-      company: "Energy Formation (ENGIE Group)",
-      position: "Web Developer/Project Manager",
-      period: "Oct 2011 - Jan 2015",
-      location: "Nantes, France",
-      activities: [
-        "Creation of a web application based on the Symfony Framework (expose/consume API)",
-        "Lead a project which aims to reduce paper consumption by supplying dematerialized documentation for customers",
-        'Monitor project that empowers technical people of Storengy. Project awarded by the "transverse award (prix transverse)" at innovation challenge of Storengy',
+        "enim eaque consequuntur, adipisci aperiam harum laboriosam reprehenderit",
+        "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum",
       ],
     },
   ],
 
   education: [
     {
-      school: "ENI Ecole Informatique",
-      degree: "Architecte Logiciel / Software Architect title (master degree)",
-      period: "2013 - 2015",
-      location: "St Herblain, France",
-      detail: [],
+      school: "University of California",
+      degree: "Computer Science (Minor)",
+      period: "2015",
+      location: "CA, US",
+      detail: [
+        "Studied speech recognition."
+      ],
     },
     {
-      school: "ENI Ecole Informatique",
-      degree:
-        "Concepteur Développeur Informatique / IT developer designer (bachelor degree)",
-      period: "2011 - 2013",
-      location: "St Herblain, France",
-      detail: [],
-    },
-    {
-      school: "Lycée Savary de Mauléon",
-      degree: "BTS Informatique de Gestion / IT management (technical degree)",
-      period: "2009 - 2011",
-      location: "Les Sables d'Olonne, France",
+      school: "University of Oxford",
+      degree: "Mathematics and Computer Science",
+      period: "2013",
+      location: "Oxford, UK",
       detail: [],
     },
   ],
 
   languages: [
     {
-      level: "Advanced",
+      level: "Native",
       language: ["English"],
     },
     {
-      level: "Native",
-      language: ["French"],
+      level: "Advanced",
+      language: ["French", "Spanish"],
     },
     {
       level: "Intermediate",
-      language: ["Spanish"],
+      language: ["German"],
     },
   ],
 
   hobbies: `
-    Personal development, canyoning (I'm guide), running, hiking, cinema (especially Danish), 
-    cooking, traveling.
+    Cooking, Hockey and Base Jump.
   `,
 }
